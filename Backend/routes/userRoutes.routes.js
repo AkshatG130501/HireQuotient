7fc8fb025646cdc369aeb9cce4b307037c68eb6d
Akshat Gupta <guptaakshat370@gmai.com>
2024-04-30 22:47:01 +0530

    fix: Add import User model

--- Backend/routes/userRoutes.routes.js
+++ Backend/routes/userRoutes.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middleware/authMiddleware.middleware";
+import { getAllUsers } from "../controllers/userControllers.controllers";
 
 const userRoutes = Router();
 
