611876f928fed6e72e2ac7afd350b0adb67337ff
Akshat Gupta <guptaakshat370@gmai.com>
2024-04-30 20:41:14 +0530

    feat: Add authroutes

--- Backend/index.js
+++ Backend/index.js
@@ -5,8 +5,12 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 
 /* Configurations */
 const app = express();
+app.use(express.json());
 
 dotenv.config();
+
+/* Routes */
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
