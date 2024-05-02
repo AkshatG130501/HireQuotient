6fb425a01dadf0db13c479f503647742f8dcd3cb
Akshat Gupta <guptaakshat370@gmai.com>
2024-05-02 12:34:55 +0530

    fix: Remove openChat route

--- Backend/routes/chatRoutes.routes.js
+++ Backend/routes/chatRoutes.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middleware/authMiddleware.middleware.js";
-import { getAllChats, openChat } from "../controllers/chatControllerrs.controllers.js";
+import { getAllChats } from "../controllers/chatControllerrs.controllers.js";
 
 const chatRoutes = Router();
 
 chatRoutes.get('/getallchats', isAuthenticated, getAllChats);
-chatRoutes.post('/openchat', isAuthenticated, openChat);
 
 export default chatRoutes;
