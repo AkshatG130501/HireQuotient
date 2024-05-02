671f375a328b02c581a71008ce4949953bc736f7
Akshat Gupta <guptaakshat370@gmai.com>
2024-05-02 12:35:57 +0530

    fix: Remove sendmessage route

--- Backend/routes/messageRoutes.routes.js
+++ Backend/routes/messageRoutes.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middleware/authMiddleware.middleware.js";
-import { getAllMessages, sendMessage } from "../controllers/messageControllers.controllers.js";
+import { getAllMessages } from "../controllers/messageControllers.controllers.js";
 
 const messageRoutes = Router();
 
 messageRoutes.get('/getallmessages/:chatId', isAuthenticated, getAllMessages);
-messageRoutes.post('/sendmessage', isAuthenticated, sendMessage);
 
 export default messageRoutes;
