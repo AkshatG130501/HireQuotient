86c1f61a89bf0495f2cddb981db54802a7498b75
Akshat Gupta <guptaakshat370@gmai.com>
2024-05-02 12:30:50 +0530

    fix: Remove open chat Route

--- Backend/controllers/chatControllerrs.controllers.js
+++ Backend/controllers/chatControllerrs.controllers.js
@@ -1,5 +1,4 @@
 import Chat from "../models/chat.models.js";
-import User from "../models/user.models.js";
 
 export const getAllChats = async (req,res) => {
     const userId = req.user._id;
@@ -21,44 +20,3 @@ export const getAllChats = async (req,res) => {
 }
 
 
-export const openChat = async (req, res) => {
-    const { userId } = req.body;
-  
-    if (!userId) {
-      return res.status(400).json({ message: "User id is required" });
-    }
-  
-    let isChat = await Chat.find({
-      $and: [
-        { users: { $elemMatch: { $eq: req.user._id } } },
-        { users: { $elemMatch: { $eq: userId } } },
-      ],
-    })
-      .populate("users", "-password")
-      .populate("lastMessage");
-  
-    isChat = await User.populate(isChat, {
-      path: "lastMessage.sender",
-      select: "name email",
-    });
-  
-    if (isChat.length > 0) {
-      res.send(isChat[0]);
-    } else {
-      let chatData = {
-        chatName: "sender",
-        users: [req.user._id, userId],
-      };
-  
-      try {
-        const createdChat = await Chat.create(chatData);
-        const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
-          "users",
-          "-password"
-        );
-        res.status(200).json(FullChat);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
-    }
-  };
